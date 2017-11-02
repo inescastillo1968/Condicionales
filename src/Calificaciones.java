e0921f110d1609e980c2170d989c3edd77e0abaa
inescastillo1968 <inesmayo12345@hotmail.com>
2017-11-02 20:48:27 -0300

    creando clase de calificaciones

---
+++ src/Calificaciones.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author LAB10
+ */
+public class Calificaciones {
+    public static void main(String[] args)
+    {
+         float nota1, nota2, nota3;
+         nota1 = Float.parseFloat(JOptionPane.showInputDialog("ingrese nota"));
+         nota2 =   Float.parseFloat(JOptionPane.showInputDialog("ingrese nota"));
+         nota3 =  Float.parseFloat(JOptionPane.showInputDialog("ingrese nota"));
+         
+    }
+}
+
+
